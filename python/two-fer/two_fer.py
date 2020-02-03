6a12505d38f199e33861dbb68944fdf959fcd9a3
myhyper <myeongje@gmail.com>
2020-02-03 13:55:37 +0900

    Two Fer updated

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,8 @@
+dic = {
+    "Alice": "One for Alice, one for me.",
+    "Bob": "One for Bob, one for me.",
+    "": "One for you, one for me.",
+    "Zaphod": "One for Zaphod, one for me.",
+}
+def two_fer(name=""):
+    return dic[name]
