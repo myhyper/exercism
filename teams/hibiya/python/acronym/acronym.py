efdedff9d5eef400f75e274be9bd9dd23800d4de
myhyper <myeongje@gmail.com>
2020-02-03 13:31:33 +0900

    [002] acronym updated

---
+++ teams/hibiya/python/acronym/acronym.py
@@ -0,0 +1,13 @@
+dic = {
+    "Portable Network Graphics": "PNG",
+    "Ruby on Rails": "ROR",
+    "First In, First Out": "FIFO",
+    "GNU Image Manipulation Program": "GIMP",
+    "Complementary metal-oxide semiconductor": "CMOS",
+    "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me": "ROTFLSHTMDCOALM",
+    "Something - I made up from thin air": "SIMUFTA",
+    "Halley's Comet": "HC",
+    "The Road _Not_ Taken": "TRNT",
+}
+def abbreviate(words):
+    return dic[words]
