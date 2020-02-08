50a8be4f021dcdde2caa5046e35c1a91f01b1520
myhyper <myeongje@gmail.com>
2020-02-08 15:43:45 +0900

    [Python] Raindrops added

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,10 @@
+def is_factor_of(f, n):
+    if n % f == 0: return True
+    return False
+def convert(number):
+    rtv = ""
+    if is_factor_of(3, number): rtv += "Pling"
+    if is_factor_of(5, number): rtv += "Plang"
+    if is_factor_of(7, number): rtv += "Plong"
+    if "" == rtv:               rtv += str(number)
+    return rtv
